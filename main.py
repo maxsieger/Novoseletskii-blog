c201f875abcdd9fa100640a089533688dd6950e8
maxsieger <maksnov10@mail.ru>
2025-07-13 18:10:48 +0300

    update main.py

--- main.py
+++ main.py
@@ -192,5 +192,5 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))ы
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port) 
